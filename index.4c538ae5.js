4492fdff9574e86c8c8ee67c3e9852fa9838f87d
Bohdan Liutyi <liutyibohdan13@gmail.com>
2024-05-15 22:04:44 +0300

    make build

---
+++ index.4c538ae5.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".tree");document.querySelectorAll("li").forEach(function(e){var n=document.createElement("span");e.prepend(n),n.append(n.nextSibling)}),e.addEventListener("click",function(e){if("SPAN"===e.target.tagName){var n=e.target.nextSibling;n&&(n.hidden=!n.hidden)}});
+//# sourceMappingURL=index.4c538ae5.js.map
